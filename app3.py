23862c22588a34faba906cf27dbada8ff455b349
karumoon <126152236+karumoon@users.noreply.github.com>
2023-02-23 19:47:29 +0900

    Create app3.py

---
+++ app3.py
@@ -0,0 +1,67 @@
+import gradio as gr
+
+from gradio_canny2image import create_demo as create_demo_canny
+from gradio_depth2image import create_demo as create_demo_depth
+from gradio_fake_scribble2image import create_demo as create_demo_fake_scribble
+from gradio_hed2image import create_demo as create_demo_hed
+from gradio_hough2image import create_demo as create_demo_hough
+from gradio_normal2image import create_demo as create_demo_normal
+from gradio_pose2image import create_demo as create_demo_pose
+from gradio_scribble2image import create_demo as create_demo_scribble
+from gradio_scribble2image_interactive import \
+    create_demo as create_demo_scribble_interactive
+from gradio_seg2image import create_demo as create_demo_seg
+from model import (DEFAULT_BASE_MODEL_FILENAME, DEFAULT_BASE_MODEL_REPO,
+                   DEFAULT_BASE_MODEL_URL, Model)
+
+with gr.Blocks(css='style.css') as demo:
+    with gr.Tabs():
+        with gr.TabItem('Canny'):
+            create_demo_canny(model.process_canny, max_images=MAX_IMAGES)
+        with gr.TabItem('Hough'):
+            create_demo_hough(model.process_hough, max_images=MAX_IMAGES)
+        with gr.TabItem('HED'):
+            create_demo_hed(model.process_hed, max_images=MAX_IMAGES)
+        with gr.TabItem('Scribble'):
+            create_demo_scribble(model.process_scribble, max_images=MAX_IMAGES)
+        with gr.TabItem('Scribble Interactive'):
+            create_demo_scribble_interactive(
+                model.process_scribble_interactive, max_images=MAX_IMAGES)
+        with gr.TabItem('Fake Scribble'):
+            create_demo_fake_scribble(model.process_fake_scribble,
+                                      max_images=MAX_IMAGES)
+        with gr.TabItem('Pose'):
+            create_demo_pose(model.process_pose, max_images=MAX_IMAGES)
+        with gr.TabItem('Segmentation'):
+            create_demo_seg(model.process_seg, max_images=MAX_IMAGES)
+        with gr.TabItem('Depth'):
+            create_demo_depth(model.process_depth, max_images=MAX_IMAGES)
+        with gr.TabItem('Normal map'):
+            create_demo_normal(model.process_normal, max_images=MAX_IMAGES)
+
+    with gr.Accordion(label='Base model', open=False):
+        current_base_model = gr.Text(label='Current base model',
+                                     value=DEFAULT_BASE_MODEL_URL)
+        with gr.Row():
+            base_model_repo = gr.Text(label='Base model repo',
+                                      max_lines=1,
+                                      placeholder=DEFAULT_BASE_MODEL_REPO,
+                                      interactive=ALLOW_CHANGING_BASE_MODEL)
+            base_model_filename = gr.Text(
+                label='Base model file',
+                max_lines=1,
+                placeholder=DEFAULT_BASE_MODEL_FILENAME,
+                interactive=ALLOW_CHANGING_BASE_MODEL)
+        change_base_model_button = gr.Button('Change base model')
+        gr.Markdown(
+            '''- You can use other base models by specifying the repository name and filename.
+The base model must be compatible with Stable Diffusion v1.5.''')
+
+    change_base_model_button.click(fn=model.set_base_model,
+                                   inputs=[
+                                       base_model_repo,
+                                       base_model_filename,
+                                   ],
+                                   outputs=current_base_model)
+
+demo.queue(api_open=False).launch()
