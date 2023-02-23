7d7bb90335cbd5bd65a309ea8abd8753a387b681
karumoon <126152236+karumoon@users.noreply.github.com>
2023-02-23 20:49:42 +0900

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,6 +5,9 @@ import pathlib
 import shlex
 import subprocess
 
+from PIL import Image                                                                                                                                                                                                                         
+import random
+
 
 if os.getenv('SYSTEM') == 'spaces':
     with open('patch') as f:
@@ -49,8 +52,8 @@ ips = [
             image_resolution, detect_resolution, ddim_steps, scale, seed, eta
         ]
 
-img=False
-model.process_pose(input_image=img,
+img=image.open("poose01.png")
+result=model.process_pose(input_image=img,
                    prompt="focus ass",
                    a_prompt="girl",
                    n_prompt="bad anatomy",
@@ -63,6 +66,33 @@ model.process_pose(input_image=img,
                    eta=0.0
 )
 
+m_dir="/content/drive/MyDrive/aipic004/"
+m_dir="./"
+def randStr():
+  arrS1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]
+  arrS1.extend(["q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6"])
+  str=""
+  str+=random.choice(arrS1)+random.choice(arrS1)+random.choice(arrS1)+random.choice(arrS1)+random.choice(arrS1)
+  return str
+
+global m_num
+m_num=0
+def image_grid(imgs, rows=2, cols=3): 
+    global m_num
+    m_num += 1                                                                                                                                                                                                        
+    w, h = imgs[0].size                                                                                                                                                                                                                       
+    grid = Image.new('RGB', size=(cols*w, rows*h))                                                                                                                                                                                            
+                                                                                                                                                                                                                                              
+    for i, img in enumerate(imgs):                                                                                                                                                                                                            
+        grid.paste(img, box=(i%cols*w, i//cols*h))  
+    fn=m_dir+"abcd__"+str(m_num)+"__"+randStr()+".jpg"
+    print(fn)
+    grid.save(fn,"jpeg")                                                                                                                                                                                          
+    return grid     
+
+print(result)
+print(result.length)
+img_grid(result)
 
 
     
