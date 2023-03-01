9bf5d3b01ac16bf19e19ab827f523eb12a18d3c7
karumoon <126152236+karumoon@users.noreply.github.com>
2023-03-02 06:39:33 +0900

    Update app3.py

--- app3.py
+++ app3.py
@@ -143,25 +143,36 @@ model.momel=model.model.to("cuda:0")
 #print(model.model.unet)
 
 print("dddddddddddddddddddd3")
-
 img=np.asanyarray(m_imArr[0])
-pt01="<lora:slavekiniv1.5:1><lora:hiqcg_body_768_epoch-000005:0.5>, hiqcgbody,hiqcgface,girl,asdfasdfer sd04"
-pt01="<lora:slavekiniv1.5:1>manasdfasdfer sd05"
-pt01="06,1girl, zelda \(the_legend_of_zelda\) wearing a slavekini, long hair, solo, crouching, best quality, masterpiece, highly detailed, intricate details, detailed face, detailed eyes, <lora:slavekiniv1.5:1>"
-nnpt01="(monochrome:1.3), (oversaturated:1.3), bad hands, lowers, 3d render, cartoon, long body, wide hips, narrow waist, disfigured, ugly, cross eyed, squinting, grain, Deformed, blurry, bad anatomy, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, disgusting, poorly drawn, mutilated, , mangled, old, surreal, ((text))"
-rett=model.process_pose_user(input_image=img,
+import random
+
+m_dir="/content/drive/MyDrive/aipic0ad/"
+def randStr():
+  arrS1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"]
+  arrS1.extend(["q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6"])
+  str=""
+  str+=random.choice(arrS1)+random.choice(arrS1)+random.choice(arrS1)+random.choice(arrS1)+random.choice(arrS1)
+  return str
+
+def func001():
+  while True:
+    pt01="<lora:slavekiniv1.5:1><lora:hiqcg_body_768_epoch-000005:0.5>, hiqcgbody,hiqcgface,girl,asdfasdfer sd04"
+    pt01="<lora:slavekiniv1.5:1>manasdfasdfer sd05"
+    pt01=randStr+",sfw,1girl, zelda \(the_legend_of_zelda\) wearing a slavekini, long hair, solo, crouching, best quality, masterpiece, highly detailed, intricate details, detailed face, detailed eyes, <lora:slavekiniv1.5:1>"
+    nnpt01="(monochrome:1.3), (oversaturated:1.3), bad hands, lowers, 3d render, cartoon, long body, wide hips, narrow waist, disfigured, ugly, cross eyed, squinting, grain, Deformed, blurry, bad anatomy, poorly drawn face, mutation, mutated, extra limb, ugly, poorly drawn hands, missing limb, floating limbs, disconnected limbs, malformed hands, blur, out of focus, long neck, disgusting, poorly drawn, mutilated, , mangled, old, surreal, ((text))"
+    rett=model.process_pose_user(input_image=img,
                    prompt=pt01,
                    a_prompt="",
                    n_prompt=nnpt01,
                    num_samples=1,
                    ddim_steps=20,
-                   image_resolution=768,
-                   detect_resolution=768,
+                   image_resolution=1024,
+                   detect_resolution=1024,
                    scale=10,
                    seed=-1,
                    eta=0.0,
                    temp=0.0,imgUser01=None)
-Image.fromarray(rett['r0'][1]).save("sd06.jpg","jpeg")
+    Image.fromarray(rett['r0'][1]).save(m_dir+"sd06.jpg","jpeg")
 
 
 """
